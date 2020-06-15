80b220b2c929533ebcfd5751ffdac16d99e35880
Athos Henrique <49050906+athdsantos@users.noreply.github.com>
2020-06-15 20:13:01 -0300

    Update api-dolar.py

--- python-api-dolar/api-dolar.py
+++ python-api-dolar/api-dolar.py
@@ -8,16 +8,16 @@ def get_requisicao():
     if 200 >= req.status_code <= 299:
       return_information()
     else:
-      print('ERRO! The site is down!')
-  except Exception as erro:
-    print(f'ERRO! {erro}')
+      print('ERROR! The site is down!')
+  except Exception as error:
+    print(f'ERROR! {error}')
 
 
 def return_information():
   try:
-    moeda_d = input('Moeda_d: ')
-    moeda_r = input('Moeda_r: ')
-    req = get('https://economia.awesomeapi.com.br/json/all/' + moeda_d + '-' + moeda_r)
+    coin_d = input('Coin d: ')
+    coin_r = input('Coin_r: ')
+    req = get('https://economia.awesomeapi.com.br/json/all/' + coind_d + '-' + coin_r)
     print('Buscando as informações..')
     sleep(2)
     for_dict = loads(req.text)
@@ -27,7 +27,7 @@ def return_information():
     print(f'High value: R${for_dict["USD"]["high"]}')
     print(f'Low value: R${for_dict["USD"]["low"]}')
     print(f'Create date: {for_dict["USD"]["create_date"]}')
-  except Exception as erro:
-    print(f'ERRO! {erro}')
+  except Exception as error:
+    print(f'ERROR! {error}')
 
 get_requisicao()
