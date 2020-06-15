b7f0e658f809c03e1f54cc0a9c639c05445031ba
Athos Henrique <49050906+athdsantos@users.noreply.github.com>
2020-06-15 20:38:07 -0300

    Update student.py

--- python-oo/pratica-python-oo/student.py
+++ python-oo/pratica-python-oo/student.py
@@ -2,9 +2,9 @@ from person import Person
 
 class Student:
 
-  def __init__(self, person, class):
+  def __init__(self, person, class_student):
     self.person = person
-    self.class = class
+    self.class_student = class_student
 
   def __str__(self):
-    return f'Student: {self.person.name}\nAge: {self.person.age}\Class: {self.class}\n'
+    return f'Student: {self.person.name}\nAge: {self.person.age}\Class: {self.class_student}\n'
