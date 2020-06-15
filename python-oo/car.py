e63720119d0182426f6ac8b798680a6d31c0f307
Athos Henrique <49050906+athdsantos@users.noreply.github.com>
2020-06-15 20:25:02 -0300

    Update and rename carro.py to car.py

---
+++ python-oo/car.py
@@ -0,0 +1,14 @@
+from veiculo import Vehicle
+
+class Car(Vehicle):
+
+    def __init__(self, color, brand, tank, wheels=4):
+      Vehicle.__init__(self, color, brand, tank, wheels)
+
+    def to_fuel(self, quantity_liters):
+      if self.tank + quantity_liters > 100:
+        print(f'Number of liters greater than the tank.')
+      else:
+        self.tank += quantity_liters
+        print(f'Fueled tank {quantity_liters}.')
+          
