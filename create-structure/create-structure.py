6e81374316d5420a14a328285bbd48fdea89bcb1
Athos Henrique <athdsantos@gmail.com>
2020-07-27 20:31:24 -0300

    add return for user

--- create-structure/create-structure.py
+++ create-structure/create-structure.py
@@ -2,22 +2,26 @@ import os
 
 def createFoldersDevWeb():
   folder = input('Folder name: ')
-  os.makedirs(f'your_path\{folder}\css')
-  open(f'your_path\{folder}\css\style.css', '+w').close()
-  os.makedirs(f'your_path\{folder}\js')
-  os.makedirs(f'your_path\{folder}\images')
-  open(f'your_path\{folder}\js\script.js', '+w').close()
-  open(f'your_path\{folder}\index.html', '+w').close()
-  os.chdir(f'your_path\{folder}')
+  os.makedirs(f'your_path_here{folder}\css')
+  open(f'your_path_here{folder}\css\style.css', '+w').close()
+  os.makedirs(f'your_path_here{folder}\js')
+  os.makedirs(f'your_path_here{folder}\images')
+  open(f'your_path_here{folder}\js\script.js', '+w').close()
+  open(f'your_path_here{folder}\index.html', '+w').close()
+  os.chdir(f'your_path_here{folder}')
+  print(f'Folder "{folder}" created with success!')
+  print(os.listdir(f'your_path_here{folder}'))
   os.system(command='code .')
 
 
 def createFoldersBasicPython():
   folder = input('Folder name: ')
-  os.mkdir(f'your_path\{folder}')
+  os.mkdir(f'your_path_here{folder}')
   fileName = input('Type the file name: ')
-  open(f'your_path\{folder}\{fileName}.py', '+w').close()
-  os.chdir(f'your_path\{folder}')
+  open(f'your_path_here{folder}\{fileName}.py', '+w').close()
+  os.chdir(f'your_path_here{folder}')
+  print(f'Folder "{folder}" created with success!')
+  print(os.listdir(f'your_path_here{folder}'))
   os.system(command='code .')
 
 
